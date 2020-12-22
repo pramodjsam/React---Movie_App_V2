ae47ab9ee737070f12d150fe59a61fc83e85ec28
Pramod Jacob Sam <pramodjsam@gmail.com>
2020-12-22 22:09:48 +0530

    Init commit

---
+++ src/components/Popup.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Popup=({selected,closePopup})=>{
+	console.log(selected)
+	return(
+		<section className='popup'>
+			<div className='content'>
+				<h2>{selected.Title}<span>({selected.Year})</span></h2>
+				<p className='rating'>Rating: {selected.imdbRating}</p>
+				<div className='plot'>
+					<img src={selected.Poster} alt="popupimg" />
+					<p>{selected.Plot}</p>
+				</div>
+				<button className='close' onClick={closePopup}>Close</button>
+			</div>
+		</section>
+	)
+}
+
+export default Popup
